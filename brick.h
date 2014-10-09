1b2ace229cc612aec1727bc4cf89af3faff7d79e
Nicole Chang <powernicole2@yahoo.com.tw>
2014-10-09 19:11:43 +0800

    Ver. 2
    Complete :
    read the cosine, i.e., the 1st and 2nd element of the matrixStr
    To-Do :
    rotate the LEGO plates and LEGO bricks

---
+++ brick.h
@@ -0,0 +1,20 @@
+
+struct brick{
+
+    int ID;
+	char btype[20];
+	int x;
+	int y;
+	int z;
+	int nbNum; /*numbers of neighbor*/
+	int stubXNum;
+	int stubZNum;
+	int holeXNum;
+	int holeZNum;
+	int heightY;
+	int dpdt_on[30];
+	int prior_to[30]; //"is dependent upon by"
+	int dpdtCount;
+	int priorCount;
+    
+};   /*   END brick   */
---
+++ brickType.h
@@ -0,0 +1,34 @@
+int setBrickType_1x2 ( int ID );
+int setBrickType_2x4 ( int ID );
+int setBrickType_2x2 ( int ID );
+int setPlateType_1x4_clockY ( int ID ); //rotate Y clockwise 順時針
+
+int setBrickType_1x2 ( int ID )
+{
+
+
+    return 0;
+}
+int setBrickType_2x4 ( int ID )
+{
+
+
+    return 0;
+}
+int setBrickType_2x2 ( int ID )
+{
+/***有空再用這個 現在無法 因為 bricks[]  這個矩陣沒有在 this.標頭檔內宣告***/   
+	/*
+	bricks[ID].holeXNum = 2;
+	bricks[ID].holeZNum = 2;
+	bricks[ID].stubXNum = 2;
+	bricks[ID].stubZNum = 2;
+	*/
+
+    return 0;
+}
+
+int setPlateType_1x4_clockY ( int ID ) // 3710.dat
+{
+	return 0;
+}
